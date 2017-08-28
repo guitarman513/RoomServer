d0fb5937b1152ba790800b40d6e1bad9ed6b4a0d
Joseph Mulhern <jmulhern@JoeMac.local>
2017-08-28 10:14:05 -0400

    fixed MQTT

--- webserver1.py
+++ webserver1.py
@@ -1,36 +1,31 @@
 from flask import Flask, render_template
+from flask_mqtt import Mqtt
+from flask_socketio import SocketIO
+
 
 app = Flask(__name__)
+app.config['MQTT_BROKER_URL'] = 'localhost'  # use the free broker from HIVEMQ
+app.config['MQTT_BROKER_PORT'] = 1883  # default port for non-tls connection
+#app.config['MQTT_USERNAME'] = ''  # set the username here if you need authentication for the broker
+#app.config['MQTT_PASSWORD'] = ''  # set the password here if the broker demands authentication
+app.config['MQTT_KEEPALIVE'] = 5  # set the time interval for sending a ping to the broker to 5 seconds
+app.config['MQTT_TLS_ENABLED'] = False  # set TLS to disabled for testing purposes
 
-#CONSTANTS FOR TAB AND URL NAMES
-tab1 = 'Lights'
-tab2 = 'VLC'
-tab3 = 'Door Lock'
+mqtt = Mqtt(app)
+socketio = SocketIO(app)
+
 
 
 
 @app.route('/')
-def index():
+def main():
     return render_template('html/main.html')
 
-#lights
+#VLC
 @app.route('/VLC/')
 @app.route('/vlc/')
 def firstTab():
     return render_template('html/VLC.html')
-
-#Door Lock
-@app.route('/VLC/')
-@app.route('/vlc/')
-def secondTab():
-    return render_template('vlc.html')
-
-#T.V.
-@app.route('/doorlock/')
-@app.route('/DoorLock/')
-def thirdTab():
-    return render_template('doorLocks.html')
-
 
 
 ############### 404 page
@@ -38,19 +33,16 @@ def thirdTab():
 def page_not_found(e):
     return render_template('html/notFound.html'), 404
 
+##################################################################################
+@app.route("/LED/color/<color>")
+def action(color):
 
+    message = color
+    mqtt.publish("LED/color",message)
 
+    return render_template('html/main.html')
 
-
-
-
-
-
-
-
-
-
-
+##################################################################################
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    socketio.run(app, debug=True, host='0.0.0.0')
